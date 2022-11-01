ebfec8ba056302f4b73603d1e871b7075e4343d7
akhil <akhil@nandhakumar.net>
2022-11-01 10:44:31 -0700

    route to codehoot page

--- src/main/java/com/nighthawk/spring_portfolio/controllers/codehoots.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/codehoots.java
@@ -1,17 +1,17 @@
-// package com.nighthawk.spring_portfolio.controllers;
+package com.nighthawk.spring_portfolio.controllers;
 
-// import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-// @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-// public class Codehoots {
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+public class Codehoots {
 
-//     // CONTROLLER handles GET request for /birds, maps it to birds() method
-//     @GetMapping("/codehoots")
-//     public String codehoots() {
+    // CONTROLLER handles GET request for /birds, maps it to birds() method
+    @GetMapping("/codehoots")
+    public String codehoots() {
 
-//         // load HTML VIEW (birds.html)
-//         return "codehoots";
+        // load HTML VIEW (birds.html)
+        return "codehoots";
 
-//     }
-// }
+    }
+}
